12d88b2b3279ad4ea21ed07921e0f724fbea6a19
anurag1302 <anurag3487@gmail.com>
2024-08-18 14:25:48 +0530

    ascii to bytes

--- ascii.js
+++ ascii.js
@@ -1,3 +1,4 @@
+//Bytes to Ascii
 const bytes = new Uint8Array([72, 101, 108, 108, 111]); // Corresponds to "Hello"
 
 function convertBytesToAscii(arr) {
@@ -10,3 +11,33 @@ function convertBytesToAscii(arr) {
 }
 
 console.log(convertBytesToAscii(bytes));
+
+function convertBytesToAsciiUsingTextDecoder(arr) {
+  return new TextDecoder().decode(arr);
+}
+
+console.log(convertBytesToAsciiUsingTextDecoder(bytes));
+
+// ***********************************
+
+//Ascii to Bytes Array
+const text = "Hello";
+
+function convertAsciiToBytes(text) {
+  let arr = [];
+  for (let i = 0; i < text.length; i++) {
+    let char = text[i];
+    arr.push(char.charCodeAt(0));
+  }
+  return arr;
+}
+
+console.log(convertAsciiToBytes(text));
+
+function convertAsciiToUInt8Array(text) {
+  //return new Uint8Array(Array.from(text).map((letter) => letter.charCodeAt(0)));
+  const resEncodedMessage = new TextEncoder().encode(text);
+  return resEncodedMessage;
+}
+
+console.log(convertAsciiToUInt8Array(text));
