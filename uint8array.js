573dbec198f72b1fdc79d52dbfcff678e6792995
anurag1302 <anurag3487@gmail.com>
2024-08-19 00:06:30 +0530

    explanation of uint8array

--- uint8array.js
+++ uint8array.js
@@ -7,3 +7,8 @@ let uint8Arr = new Uint8Array([0, 255, 127, 128]);
 uint8Arr[1] = 299;
 
 console.log(uint8Arr);
+//here 299 is converted to binary first- 100101011, it has 9 digits.
+//since uint8Array only stores 8 bits, it takes 8 digits from the right
+//which is equal to 00101011
+//the decimal equivalent of 00101011 is 43
+//hence when we log the new array, 43 is printed
